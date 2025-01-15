bd7c92f3a7069b9435f45888db619008c5959f42
kaylanysantos <kaylanycorre45@gmail.com>
2025-01-15 15:39:33 -0300

    socorro

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -1,11 +1,18 @@
 import Header from '../Header'
 import './style.css'
+import asaslivres from "../../assets/cd_cover.webp"
+
 
 export default function App() {
   return (
     <>
       <Header title="Tocando Agora" />
       <h1>Olá mundo</h1>
+      <div>
+    <img className="w-80 h-80 rounded-3xl" src={asaslivres} alt="" />
+    <h1 className="font-bold" >TUDO AZUL</h1>
+    <h2>asas livres</h2>
+  </div>
     </>
   )
 }
